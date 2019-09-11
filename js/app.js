fedbaa4b2cc6fdd946fcd4466ae80d0a3f9598f8
Tlabrahamson <tlabrahamson@yahoo.com>
2019-09-10 22:51:21 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.navLinks');
+const links = document.querySelectorAll('.navLinks li');
+
+hamburger.addEventListener('click', () =>{
+    navLinks.classList.toggle('open');
+    links.forEach(link =>{
+        link.classList.toggle('fade');
+    });
+});
